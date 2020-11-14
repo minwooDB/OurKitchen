08032b08aa7c6f14e83482b6ada671829f3b2b73
ksom95 <ksom95@gmail.com>
2020-11-14 15:26:46 +0900

    lombok, BizCode Enum

--- src/main/java/com/ourkitchen/auth/vo/User.java
+++ src/main/java/com/ourkitchen/auth/vo/User.java
@@ -8,7 +8,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name="user")
 public class User {
@@ -28,48 +32,5 @@ public class User {
 	@Enumerated(EnumType.STRING)
 	@Column(name="classification")
 	private Classification classification;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getPhoneNum() {
-		return phoneNum;
-	}
-	public void setPhoneNum(String phoneNum) {
-		this.phoneNum = phoneNum;
-	}
-	public Classification getClassification() {
-		return classification;
-	}
-	public void setClassification(Classification classification) {
-		this.classification = classification;
-	}
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", email=" + email + ", name=" + name + ", password=" + password + ", phoneNum="
-				+ phoneNum + ", classification=" + classification + "]";
-	}
-	
 	
 }
--- src/main/resources/db/migration/V2011011600__create_Reservation_table.sql
+++ src/main/resources/db/migration/V2011011600__create_Reservation_table.sql
@@ -5,8 +5,8 @@ CREATE TABLE RESERVATION (
 	start_date DATETIME NOT NULL,
 	end_date DATETIME NOT NULL,
 	time ENUM('M', 'L','D'),
-	category VARCHAR(2)
-	status ENUM('0', '1','2'),
+	category VARCHAR(2),
+	status ENUM('0', '1', '2'),
 	FOREIGN KEY(kitchen_id) REFERENCES KITCHEN_INFO(id),
 	FOREIGN KEY(user_id) REFERENCES USER(id)
 )
